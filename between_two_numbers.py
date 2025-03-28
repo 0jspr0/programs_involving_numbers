c8325664f260e49f3045c61565ae62b7c156c0d5
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-28 23:58:47 +0800

    Added if and elif statements to identify the lower number for start and the higher number increased by 1 for stop from the 2 integer input variables and display the numbers between them

--- between_two_numbers.py
+++ between_two_numbers.py
@@ -1,2 +1,9 @@
 number1 = int(input("Enter number 1: "))
 number2 = int(input("Enter number 2: "))
+
+if number1 < number2:
+	for i in range(number1, number2+1):
+		print(i)
+elif number2 < number1:
+	for i in range(number2, number1+1):
+		print(i)
