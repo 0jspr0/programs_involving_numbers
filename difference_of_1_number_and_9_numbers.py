74a04925276f85f34d8d6771aa3cfca62e8e561e
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-28 23:31:39 +0800

    Initialized a variable for sum and used the + operator to get the sum and used the - operator to get the difference and display it

--- difference_of_1_number_and_9_numbers.py
+++ difference_of_1_number_and_9_numbers.py
@@ -1,4 +1,11 @@
 number1 = int(input("Enter number 1: "))
 
+sum = 0
+
 for i in range(9):
 	number = int(input(f"Enter number {i + 2}: "))
+	sum = sum + number
+	
+difference = number1 - sum
+
+print(difference)
