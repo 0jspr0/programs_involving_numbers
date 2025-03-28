ae53680f7caa9e5bb0e79865f9f608f9f41824a3
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-28 23:35:38 +0800

    Initialized a variable for the number of even numbers and added an if statement to identify if the number has a remainder of 0 to increase the number of even numbers by 1 and display it

--- number_of_even_numbers.py
+++ number_of_even_numbers.py
@@ -1,2 +1,8 @@
+number_of_even_numbers = 0
+
 for i in range(10):
 	number = int(input(f"Enter number {i + 1}: "))
+	if number % 2 == 0:
+		number_of_even_numbers += 1
+
+print(f"Number of even numbers: {number_of_even_numbers}")
