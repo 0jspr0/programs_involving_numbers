70e71ca557a28a560e2c1a90fa4bea5dda17af96
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-28 20:34:28 +0800

    Initialized a variable for the number of odd numbers and added an if statement to identify if the number has a remainder of 1 to increase the number of odd numbers by 1 and display it

--- number_of_odd_numbers.py
+++ number_of_odd_numbers.py
@@ -1,2 +1,8 @@
+number_of_odd_numbers = 0
+
 for i in range(10):
 	number = int(input(f"Enter number {i + 1}: "))
+	if number % 2 == 1:
+		number_of_odd_numbers += 1
+
+print(f"Number of odd numbers: {number_of_odd_numbers}")
