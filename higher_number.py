7279c2d9073da07a3a19d513b861f88c67030968
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-28 18:43:18 +0800

    Added if and elif statements to identify the higher number from the 2 integer input variables and display it

--- higher_number.py
+++ higher_number.py
@@ -1,2 +1,7 @@
 number1 = int(input("Enter number 1: "))
 number2 = int(input("Enter number 2: "))
+
+if number1 > number2:
+	print(f"Higher number: {number1}")
+elif number2 > number1:
+	print(f"Higher number: {number2}")
