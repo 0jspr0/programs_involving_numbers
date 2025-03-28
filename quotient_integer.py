b9327e9160057556beb1409777a8541e98b879c5
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-28 23:01:52 +0800

    Used the / operator to get the quotient and converted it into integer and display it

--- quotient_integer.py
+++ quotient_integer.py
@@ -1,2 +1,6 @@
 number1 = int(input("Enter number 1: "))
 number2 = int(input("Enter number 2: "))
+
+quotient = number1 / number2
+quotient_integer = int(quotient)
+print(f"Quotient integer: {quotient_integer}")
