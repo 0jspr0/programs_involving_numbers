68bd611dd248368ee7565e08a3b2fee2292808a8
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-30 21:11:42 +0800

    Initialized a list for numbers with no duplicate numbers and added a for statement for an if statement to identify if a number in the unique number list is not in the duplicate number list and added a for statement to display each iteration in the with no duplicate number list

--- with_no_duplicate_list.py
+++ with_no_duplicate_list.py
@@ -1,6 +1,8 @@
 unique_list = []
 duplicate_list = []
 
+with_no_duplicate_list = []
+
 for i in range(10):
 	number = int(input(f"Enter number {i + 1}: "))
 	
@@ -8,3 +10,10 @@ for i in range(10):
 		unique_list.append(number)
 	else:
 		duplicate_list.append(number)
+
+for i in unique_list:
+	if i not in duplicate_list:
+		with_no_duplicate_list.append(i)
+
+for i in with_no_duplicate_list:
+	print(i)
