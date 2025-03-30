d58b39454baec50fecbb980600cb651d0be08ef1
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-30 20:52:14 +0800

    Initialized lists for unique numbers and duplicate numbers and added an if statement to identify if a number is not in the unique number list to add it in the unique number list, otherwise add it in the duplicate number list

--- with_no_duplicate_list.py
+++ with_no_duplicate_list.py
@@ -1,2 +1,10 @@
+unique_list = []
+duplicate_list = []
+
 for i in range(10):
 	number = int(input(f"Enter number {i + 1}: "))
+	
+	if number not in unique_list:
+		unique_list.append(number)
+	else:
+		duplicate_list.append(number)
