16d3ac14bed63a1c2c34d8daf781ecd2c5994145
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-31 20:12:48 +0800

    Added an if statement to identify if a number is not in the unique number list to add it in the unique number list and display the text unique, otherwise add it in the duplicate number list and display the text duplicate

--- unique_or_duplicate.py
+++ unique_or_duplicate.py
@@ -1,8 +1,17 @@
 numbers = 0
 
+unique_list = []
+duplicate_list = []
+
 while True:
 	numbers += 1
 	try:
 		number = int(input(f"Enter number {numbers}: "))
+		if number not in unique_list:
+			unique_list.append(number)
+			print("Unique")
+		else:
+			duplicate_list.append(number)
+			print("Duplicate")
 	except ValueError:
 		break
